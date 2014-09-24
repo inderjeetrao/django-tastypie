54816c26dbf80869051ce10d712cf4afb371909c
inderjeet <inderjeetrao.ir@gmail.com>
2014-09-24 15:07:00 +0530

    changes for username being none in custom user model

--- tastypie/compat.py
+++ tastypie/compat.py
@@ -1,11 +1,12 @@
 from __future__ import unicode_literals
-from django.conf import settings
+# from django.conf import settings
 import django
 
-__all__ = ['get_user_model', 'get_username_field', 'AUTH_USER_MODEL']
+# __all__ = ['get_user_model', 'get_username_field', 'AUTH_USER_MODEL']
+#
+# AUTH_USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
 
-AUTH_USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
-
+__all__ = ['User']
 # Django 1.5+ compatibility
 if django.VERSION >= (1, 5):
     def get_user_model():
--- tastypie/models.py
+++ tastypie/models.py
@@ -4,7 +4,7 @@ import time
 from django.conf import settings
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
-from tastypie.utils import now
+from django.utils.timezone import now
 
 try:
     from hashlib import sha1
@@ -30,7 +30,8 @@ class ApiAccess(models.Model):
 
 if 'django.contrib.auth' in settings.INSTALLED_APPS:
     import uuid
-    from tastypie.compat import AUTH_USER_MODEL
+    # from tastypie.compat import AUTH_USER_MODEL
+    AUTH_USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
     class ApiKey(models.Model):
         user = models.OneToOneField(AUTH_USER_MODEL, related_name='api_key')
         key = models.CharField(max_length=128, blank=True, default='', db_index=True)
